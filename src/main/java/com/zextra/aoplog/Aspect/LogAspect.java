b222b9fa9bcf9a568aae69fff5a93f5569d88441
zhangqi71 <zhangqi71@baidu.com>
2022-09-09 17:33:17 +0800

    Initial commit

---
+++ src/main/java/com/zextra/aoplog/Aspect/LogAspect.java
@@ -0,0 +1,20 @@
+package com.zextra.aoplog.Aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Aspect
+@Component
+public class LogAspect {
+    @Before("execution(* com.zextra.aoplog.controller..*(..))")
+    public void beforeAdvice(JoinPoint point){
+        String methodName = point.getSignature().getName();
+        List<Object> args = Arrays.asList(point.getArgs());
+        System.out.println("@Before 前置通知 : 方法名 【 " + methodName + " 】and args are " + args);
+    }
+}
